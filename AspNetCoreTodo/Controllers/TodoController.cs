3c5f8e76600892755dee0bdd08e9cd4a92aa24ca
travjsmit <inivid@icloud.com>
2024-07-19 12:42:55 -0700

    First Commit

---
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreTodo.Controllers
+{
+    public class TodoController : Controller
+    {
+        private readonly ITodoItemService _todoItemService;
+
+        public TodoController(ITodoItemService todoItemService)
+        {
+            _todoItemService = todoItemService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var items = await _todoItemService.GetIncompleteItemsAsync();
+            
+            // Put items into a model
+
+            // Render view using the model
+        }
+    }
+}
