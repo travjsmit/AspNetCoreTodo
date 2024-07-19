a37dddaa88732f89fe7c6f969f6146f56c10f17e
travjsmit <inivid@icloud.com>
2024-07-19 13:38:50 -0700

    Corrected tabs and curlys in HomeController.cs

--- AspNetCoreTodo/obj/Debug/net8.0/AspNetCoreTodo.AssemblyInfo.cs
+++ AspNetCoreTodo/obj/Debug/net8.0/AspNetCoreTodo.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AspNetCoreTodo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e63535e0d1bfcdaf953ac1476d152b9ec82b5b0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a96489260d7304a5d49406fe40ea84597253382")]
 [assembly: System.Reflection.AssemblyProductAttribute("AspNetCoreTodo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AspNetCoreTodo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
